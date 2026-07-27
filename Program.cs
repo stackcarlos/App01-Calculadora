e2d28ae5a1323f3b11a21c746d7a329fff1ad1cf
Carlos Santos <danielsilvae790@gmail.com>
2026-07-27 16:42:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace teste1
+{
+    class Program
+    {
+
+    
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá " + nome + "!");
+          
+       
+        }
+    }
+}
